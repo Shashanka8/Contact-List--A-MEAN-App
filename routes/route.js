62694f3b8eafb6873e4266ddabcc5b2c329cb1bd
Shashanka Shekhar Nayak <44174922+Shashanka8@users.noreply.github.com>
2019-01-02 14:50:44 +0530

    Added server side code

---
+++ routes/route.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+const Contact = require('../models/contacts');
+
+//retrieving data
+router.get('/contacts', (req, res, next) => {
+    Contact.find(function(err, contacts) {
+        res.json(contacts);
+    })
+});
+
+//add contacts
+router.post('/contact', (req, res, next) => {
+    let newContact = new Contact({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone: req.body.phone
+    });
+
+    newContact.save((err, contact) => {
+        if(err) {
+            res.json({msg:'Faied to add contact'});
+        }
+        else {
+            res.json({msg:'Contact added successfully'});
+        }
+    });
+});
+
+//delete contacts
+router.delete('/contact/:id', (req, res, next) => {
+    Contact.remove({_id: req.params.id}, function(err, result){
+        if(err) {
+            res.json(err);
+        }
+        else {
+            res.json(result);
+        }
+    });
+});
+
+module.exports = router;
